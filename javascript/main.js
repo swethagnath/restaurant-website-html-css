92f4db39c993df6ffc4e3d157d5cb49355e94a9e
swetha.gopinath@texple.com <swethagnthn@gmail.com>
2023-11-04 14:08:11 +0530

    added javascript and index

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
+    console.log("loaded")
     $(window).on('load scroll',function(){
+        console.log("here")
         if($(window).scrollTop() > 60){
-            $('header .header-2').addClass('header-active');
+            $('.header-2').addClass('header-active');
+        }else{
+            $('.header-2').removeClass('header-active');
         }
     })
 })
